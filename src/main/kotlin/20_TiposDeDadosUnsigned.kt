2c1d3b5c162291b22f76c60c52e76816fa087a39
Rodrigo de Araujo Leite Pereira <rodrigoaraujo1709@gmail.com>
2022-09-14 22:47:06 -0300

    20. Tipos de dados - Unsigned

---
+++ src/main/kotlin/20_TiposDeDadosUnsigned.kt
@@ -0,0 +1,11 @@
+fun main(){
+
+
+    // Unsigned
+
+    println("ULong MAX - ${ULong.MAX_VALUE} - ${ULong.MIN_VALUE}")
+    println("UInt MAX - ${UInt.MAX_VALUE} - ${UInt.MIN_VALUE}")
+    println("UUShort MAX - ${UShort.MAX_VALUE} - ${UShort.MIN_VALUE}")
+    println("UByte MAX - ${UByte.MAX_VALUE} - ${UByte.MIN_VALUE}")
+    
+}
