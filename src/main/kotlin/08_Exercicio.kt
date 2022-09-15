2e4cac6d9f880232724dc0db738f1bdd5871b56b
Rodrigo de Araujo Leite Pereira <rodrigoaraujo1709@gmail.com>
2022-09-14 23:16:09 -0300

    24. Reforce seu aprendizado - Variáveis, tipos de dado e operadores

---
+++ src/main/kotlin/08_Exercicio.kt
@@ -0,0 +1,16 @@
+fun main(){
+
+    var nome: String = "Rodrigo de Araujo Leite Pereira"
+
+    var stringSemValor: String
+
+    val menorNrPossivel: Byte = 42
+
+    var pIBBrasil: Long = 1869000000000
+
+    var populacaoBrasil: Int = 211000000
+
+    println(pIBBrasil/populacaoBrasil)
+
+
+}
